c9679d1aa2ab1ca505a01fc0d98431ecd19064a9
Hernandez <lu97is@gmail.com>
2021-05-31 14:19:53 -0500

    e2e testing

---
+++ fixtures/critical/index.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture`testing`.page('https://compassionate-shaw-7d2e6f.netlify.app/')
+
+test('Works', async t => {
+    const Element = Selector('p').withText('Maste element 2');
+    await t.expect(Element.visible).ok();
+})
